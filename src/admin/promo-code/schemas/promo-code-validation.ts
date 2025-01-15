b91c9a6572adbd716715ba097a5b57590013352d
ancorframe <yura.pundiak@gmail.com>
2025-01-15 14:30:52 +0200

    Update promo-code-validation.ts

--- src/admin/promo-code/schemas/promo-code-validation.ts
+++ src/admin/promo-code/schemas/promo-code-validation.ts
@@ -65,3 +65,22 @@ export const editPromoCodeSchema = Joi.object()
     }),
   })
   .unknown(false);
+
+export const DeletePromoCodesValidationSchema = Joi.array()
+  .items(
+    Joi.string()
+      .required()
+      .messages({
+        'any.required': "id є обов'язковим полем",
+        'string.base': 'id має бути рядком',
+        'string.empty': 'id не може бути порожнім',
+      })
+      .example('6744d2c8bd8f6d722ff28c49'),
+  )
+  .min(1)
+  .required()
+  .messages({
+    'any.required': " є обов'язковим полем",
+    'array.base': ' має бути масивом',
+    'array.empty': ' не може бути порожнім',
+  });
