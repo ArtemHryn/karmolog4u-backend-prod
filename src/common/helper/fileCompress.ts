f54b62e9010171635df04b875c510d640d50974e
ancorframe <yura.pundiak@gmail.com>
2025-07-13 14:22:27 +0300

    Update fileCompress.ts

--- src/common/helper/fileCompress.ts
+++ src/common/helper/fileCompress.ts
@@ -2,18 +2,21 @@ import { ConfigService } from '@nestjs/config';
 import * as sharp from 'sharp';
 import { HttpException, InternalServerErrorException } from '@nestjs/common';
 import { fileDelete } from './fileDelete';
+import * as path from 'path';
+import * as fs from 'fs/promises';
 
 export const fileCompress = async (
   file: any,
   configService: ConfigService,
-  path = 'covers',
+  newPath = 'covers',
 ) => {
   try {
+    const rootFolder = path.join(process.cwd(), '..', 'storage', newPath);
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [name, extension] = file.filename.split('.');
     // const outputDir = path.join(__dirname, '..', '..', '..'/, 'covers');
     // const compressedPath = path.join(outputDir, `${name}.webp`);
-    const compressedPath = `${path}/${name}.webp`;
+    const compressedPath = `${rootFolder}/${name}.webp`;
 
     try {
       await sharp(file.path)
@@ -29,7 +32,8 @@ export const fileCompress = async (
 
     //crate link for image
     const envValue = configService.get<string>('SERVER_IP');
-    return `${envValue}${compressedPath}`;
+    const coverPath = path.join('covers', `${name}.webp`);
+    return `${envValue}${coverPath}`;
   } catch (error) {
     throw new HttpException(
       {
