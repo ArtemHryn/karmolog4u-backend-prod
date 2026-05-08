1db7ecdb1cbce1cd2a96bbb319fd082e485699c3
Artem <grynbest@gmail.com>
2026-05-08 21:56:11 +0200

    added events API for users

---
+++ src/events/events.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Role } from 'src/role/role.enum';
+import { Roles } from 'src/role/roles.decorator';
+import { EventsServices } from './events.service';
+import { GetAllEventsDto } from './dto/get-all-events.dto';
+
+@ApiBearerAuth()
+@ApiTags('user-events')
+@Roles(Role.User)
+@Controller('/user/events')
+export class EventsController {
+  constructor(private readonly eventsService: EventsServices) {}
+  @Get('all')
+  async getAllEvents(@Query() query: GetAllEventsDto) {
+    return await this.eventsService.getAllEvents(query);
+  }
+}
